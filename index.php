458772b66a536debb0f1d8a63a65decc8f867698
NovelV621 <60910726+NovelV621@users.noreply.github.com>
2021-06-04 00:34:54 +0700

    novel

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 <body>
 	
 	<div class="limiter">
-		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+		<div class="container-login100" style="background-image: url('images/bg-02.jpg');">
 			<div class="wrap-login100 p-t-30 p-b-50">
 				<span class="login100-form-title p-b-41">
 					Account Login
@@ -49,7 +49,7 @@
  					}
  					?>
 				</span>
-				<form action="login-proses.php" method="post" class="login100-form validate-form p-b-33 p-t-5">
+				<form action="login-proses.php" method="post" class="login100-form validate-form p-b-33 p-t-5" style="background-color: rgb(255, 255, 225,0.5);">
 
 					<div class="wrap-input100 validate-input" data-validate = "Enter username">
 						<input class="input100" type="text" name="username" placeholder="ID">

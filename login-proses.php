0c4f23b668635ad2f942b277a0c8ed50cfa0d1e7
NovelV621 <60910726+NovelV621@users.noreply.github.com>
2021-06-03 21:41:21 +0700

    commit login dll

---
+++ login-proses.php
@@ -0,0 +1,51 @@
+<?php
+// mengaktifkan session pada php
+session_start();
+
+// menghubungkan php dengan koneksi database
+include 'koneksi.php';
+
+// menangkap data yang dikirim dari form login
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// menyeleksi data user dengan username dan password yang sesuai
+$sql = mysqli_query($db,"SELECT * FROM user,mahasiswa WHERE username='$username' and user.password='$password'");
+// menghitung jumlah data yang ditemukan
+$cek = mysqli_num_rows($sql);
+
+// cek apakah username dan password di temukan pada database
+
+if($cek > 0){
+
+ $data = mysqli_fetch_assoc($sql);
+
+ // cek jika user login sebagai admin
+ if($data['level']=="admin"){
+
+  // buat session login dan username
+  $_SESSION['name'] = $data['nama'];
+  $_SESSION['username'] = $username;
+  $_SESSION['level'] = "admin";
+  // alihkan ke halaman dashboard admin
+  header("location:home.php");
+
+ // cek jika user login sebagai pegawai
+ }else if($data['level']=="mahasiswa"){
+  // buat session login dan username
+  $_SESSION['name'] = $data['nama'];
+  $_SESSION['username'] = $username;
+  $_SESSION['level'] = "mahasiswa";
+  // alihkan ke halaman dashboard pegawai
+  header("location:home.php");
+
+ }else{
+
+  // alihkan ke halaman login kembali
+  header("location:index.php?pesan=gagal");
+ } 
+}else{
+ header("location:index.php?pesan=gagal");
+}
+
+?>
---
+++ tampil_mhs.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<?php
+include('koneksi.php');
+require_once('head.php');
+
+$username = $_SESSION['username'];
+$sql = "SELECT * FROM kelas join krs on kelas.kelas_id = krs.kelas_id join mahasiswa on krs.id = mahasiswa.id WHERE mahasiswa.nim = '$username' ";
+
+
+?>
+<body>
+    <?php
+    require_once('leftpanel.php');
+    ?>
+
+    <div id="right-panel" class="right-panel">
+        <?php
+            require_once('header.php');
+        ?>
+
+        <div class="breadcrumbs">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        <h1>Dashboard</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            <li class="active"><i class="fa fa-dashboard"></i></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content mt-3">
+
+<div class="animated fadeIn">
+<div class="card">
+<div class="card-header">
+    <div class="pull-left">
+        <strong>Data Kelas</strong>
+    </div>
+</div>
+<div class="card-body table-responsive">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Kode Kelas</th>
+                <th>Kode Matkul</th>
+                <th>Nama Matkul</th>
+                <th>Tahun</th>
+                <th>Semester</th>
+                <th>Sks</th>
+                <th>Detail</th>
+        
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                if(!$result = $db->query($sql)){
+                    die('Gagal Meminta Data');
+                }
+                while($row = $result->fetch_assoc()){
+            ?>
+                <tr>
+                    <td><?php echo $row ['kode_kelas'] ?></td>
+                    <td><?php echo $row ['kode_matkul'] ?></td>
+                    <td><?php echo $row ['nama_matkul'] ?></td>
+                    <td><?php echo $row ['tahun'] ?></td>
+                    <td><?php echo $row ['semester'] ?></td>
+                    <td><?php echo $row ['sks'] ?></td>
+                    <td><a href="kelas_detail.php?kelas_id=<?= $row['kelas_id']; ?>" class="btn btn-primary">Detail</a>
+                    </td>
+                </tr>
+
+        </tbody>
+        <?php } ?>
+    </table>
+    </div>
+</div>
+</div>
+
+</div>
+            
+
+        </div>
+    </div>    
+
+</body>
+</html>

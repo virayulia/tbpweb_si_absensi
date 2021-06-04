2223caaaacb9a78dbfbbbe38206ae2e159201dc3
NovelV621 <60910726+NovelV621@users.noreply.github.com>
2021-06-04 22:38:47 +0700

    novel2

--- detail_mhs.php
+++ detail_mhs.php
@@ -4,7 +4,9 @@ include('koneksi.php');
 require_once('head.php');
 
 $username = $_SESSION['username'];
-$sql = "SELECT * FROM kelas join krs on kelas.kelas_id = krs.kelas_id join mahasiswa on krs.mahasiswa_id = mahasiswa.mahasiswa_id WHERE mahasiswa.nim = '$username' ";
+$id_kelas = $_GET['kelas_id'];
+$sql = "SELECT * FROM kelas join krs on kelas.kelas_id = krs.kelas_id join mahasiswa on krs.mahasiswa_id = mahasiswa.mahasiswa_id WHERE mahasiswa.nim = '$username' and kelas.kelas_id= '$id_kelas' ";
+$kehadiran = "SELECT * FROM absensi join pertemuan on absensi.absensi_id=pertemuan.pertemuan_id join kelas on pertemuan.kelas_id=kelas.kelas_id join krs on kelas.kelas_id = krs.kelas_id join mahasiswa on krs.mahasiswa_id=mahasiswa.mahasiswa_id WHERE mahasiswa.nim='$username' and kelas.kelas_id='$id_kelas'";
 
 
 ?>
@@ -56,7 +58,6 @@ $sql = "SELECT * FROM kelas join krs on kelas.kelas_id = krs.kelas_id join mahas
                 <th>Tahun</th>
                 <th>Semester</th>
                 <th>Sks</th>
-                <th>Detail</th>
         
             </tr>
         </thead>
@@ -74,12 +75,55 @@ $sql = "SELECT * FROM kelas join krs on kelas.kelas_id = krs.kelas_id join mahas
                     <td><?php echo $row ['tahun'] ?></td>
                     <td><?php echo $row ['semester'] ?></td>
                     <td><?php echo $row ['sks'] ?></td>
-                    <td><a href="detail_mhs.php?kelas_id=<?= $row['kelas_id']; ?>" class="btn btn-primary">Detail</a>
-                    </td>
                 </tr>
 
         </tbody>
         <?php } ?>
+    </table>
+    </div>
+</div>
+</div>
+
+<div class="animated fadeIn">
+<div class="card">
+<div class="card-header">
+    <div class="pull-left">
+        <strong>Kehadiran</strong>
+    </div>
+</div>
+<div class="card-body table-responsive">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Pertemuan Ke-</th>
+                <th>Materi</th>
+                <th>Jam Masuk</th>
+                <th>Jam Keluar</th>
+                <th>Durasi</th>
+        
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                if(!$result = $db->query($kehadiran)){
+                    die('Gagal Meminta Data');
+                }
+                while($row = $result->fetch_assoc()){
+            $i=1;?>
+
+                <tr>
+                    <td><?php echo $i; ?></td>
+                    <td><?php echo $row ['pertemuan_ke'] ?></td>
+                    <td><?php echo $row ['materi'] ?></td>
+                    <td><?php echo $row ['jam_masuk'] ?></td>
+                    <td><?php echo $row ['jam_keluar'] ?></td>
+                    <td><?php echo $row ['durasi'] ?></td>
+                </tr>
+
+
+        </tbody>
+        <?php $i++; } ?> 
     </table>
     </div>
 </div>
